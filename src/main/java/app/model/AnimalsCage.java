a24750534d17fdbb2bf7a65ae090d926b57656d5
Dmitriy <dadinoff1@gmail.com>
2023-12-20 15:48:57 +0300

    added logging

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,31 +1,35 @@
 package app.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
-    @Autowired
-    @Qualifier("dog")
-    private Animal animal;
+    private static Logger logger = LoggerFactory.getLogger(AnimalsCage.class);
+
+    private final Animal animal;
+
+    private final Timer timer;
 
     @Autowired
-    private Timer timer;
+    public AnimalsCage(@Qualifier("dog") Animal animal, Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
 
     public Timer getTimer(){
         return this.timer;
     }
 
-
-
     public void whatAnimalSay() {
-        System.out.println("Say:");
-        System.out.println(animal.toString());
-        System.out.println("At:");
-        System.out.println(getTimer().getTime());
-        System.out.println("________________________");
+        logger.info("Say:");
+        logger.info(animal.toString());
+        logger.info("At:");
+        logger.info(timer.getTime().toString());
+        logger.info("________________________");
     }
 }
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class Timer {
 
+
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
